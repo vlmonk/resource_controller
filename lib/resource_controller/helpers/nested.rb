b3a607e874351d3cacb4ed5b11754099e72eb97a
Henning Koch <henning.koch@makandra.de>
2010-10-30 10:04:32 +0200

    Revert "Fixed typo with parent_param."
    This reverts commit 5537625a5215d7cf5528aecbb724d718df30e2ca.

--- lib/resource_controller/helpers/nested.rb
+++ lib/resource_controller/helpers/nested.rb
@@ -65,9 +65,9 @@ module ResourceController
             parent_model.find(parent_param)
           else
             if parent_model.respond_to? :find_by_param!
-              parent_model.find_by_param!(parent_param)
+              parent_model.find_by_param!(param)
             elsif parent_model.respond_to? :find_by_permalink
-              parent_model.find_by_permalink(parent_param)
+              parent_model.find_by_permalink(param)
             end
           end
         end
