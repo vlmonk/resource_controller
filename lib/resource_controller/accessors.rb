ccb5e3f11840432fbbb1b84be06cfa5ceb30a6cd
Dominik Masur <dominik.masur@googlemail.com>
2012-02-19 09:29:28 +0100

    Updated for Rails 3.2

--- lib/resource_controller/accessors.rb
+++ lib/resource_controller/accessors.rb
@@ -10,16 +10,16 @@ module ResourceController # :nodoc:
                 args.push block if block_given?
                 @#{block_accessor} = [args].flatten
               end
-              
+
               @#{block_accessor}
             end
 
           end_eval
         end
       end
-      
+
       def scoping_reader(*accessor_names)
-        accessor_names.each do |accessor_name|        
+        accessor_names.each do |accessor_name|
           class_eval <<-"end_eval", __FILE__, __LINE__
             def #{accessor_name}(&block)
               @#{accessor_name}.instance_eval &block if block_given?
@@ -28,18 +28,20 @@ module ResourceController # :nodoc:
           end_eval
         end
       end
-      
+
       def class_scoping_reader(accessor_name, start_value)
-        write_inheritable_attribute accessor_name, start_value
-        
+        private_accessor_name = "_#{accessor_name}"
+        class_attribute private_accessor_name
+        self.send("#{private_accessor_name}=", start_value)
+
         class_eval <<-"end_eval", __FILE__, __LINE__
           def self.#{accessor_name}(&block)
-            read_inheritable_attribute(:#{accessor_name}).instance_eval(&block) if block_given?
-            read_inheritable_attribute(:#{accessor_name})
+            self.#{private_accessor_name}.instance_eval(&block) if block_given?
+            self.#{private_accessor_name}
           end
         end_eval
       end
-      
+
       def reader_writer(accessor_name)
         class_eval <<-"end_eval", __FILE__, __LINE__
           def #{accessor_name}(*args, &block)
@@ -49,29 +51,32 @@ module ResourceController # :nodoc:
           end
         end_eval
       end
-      
+
       def class_reader_writer(*accessor_names)
         accessor_names.each do |accessor_name|
           class_eval <<-"end_eval", __FILE__, __LINE__
+            class_attribute :_#{accessor_name}
+
             def self.#{accessor_name}(*args)
               unless args.empty?
-                write_inheritable_attribute :#{accessor_name}, args.first if args.length == 1
-                write_inheritable_attribute :#{accessor_name}, args if args.length > 1
+                self._#{accessor_name} = args.first if args.length == 1
+                self._#{accessor_name} = args if args.length > 1
               end
-            
-              read_inheritable_attribute :#{accessor_name}
+
+              self._#{accessor_name}
             end
-          
+
             def #{accessor_name}(*args)
               unless args.empty?
-                self.class.write_inheritable_attribute :#{accessor_name}, args.first if args.length == 1
-                self.class.write_inheritable_attribute :#{accessor_name}, args if args.length > 1
+                self.class._#{accessor_name} = args.first if args.length == 1
+                self.class._#{accessor_name} = args if args.length > 1
               end
-            
-              self.class.read_inheritable_attribute :#{accessor_name}
+
+              self.class._#{accessor_name}
             end
           end_eval
         end
       end
+
   end
 end
